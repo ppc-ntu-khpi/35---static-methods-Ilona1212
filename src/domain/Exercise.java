ceab7ba6690d337a6e5ec35c0f03fbd7887864a8
Ilona1212 <59233009+Ilona1212@users.noreply.github.com>
2021-01-26 16:44:47 +0200

    Update Exercise.java

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,49 @@
 package domain;
 
+/**
+ * Клас який представляє завдання
+ * @author Ilona1212
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    
+    /**
+     * Знаходить всі прості числа між двома заданими числами
+     * @param min число від якого почати пошук
+     * @param max число до якого шукати
+     * @return прості числа через кому
+     */
+    public static String Calculate(int min, int max) {
+        String s = "";
+        
+        for (int i = min; i <= max;i++){
+            if (checkSimple(i)) {
+                s += String.valueOf(i) + ", ";
+            }
+        }
+        
+        if (s.equals("")) {
+            return "numbers found";
+        }
+        
+        s = s.substring(0, s.length()-2);
+        
+        return s;
     }
+    
+    private static boolean checkSimple(int i){
+    if (i <= 1)
+        return false;
+    else if (i <= 3)
+        return true;
+    else if (i%2 == 0 || i%3 == 0)
+        return false;
+    int n = 5;
+    while (n*n <= i){
+        if (i%n == 0 || i%(n+2) == 0)
+            return false;
+        n=n+6;
+    }
+    return true;
+}
+    
 }
