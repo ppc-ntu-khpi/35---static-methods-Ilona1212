89fad1b6994f7f1b53ba113b97901c98faa975a4
Ilona1212 <59233009+Ilona1212@users.noreply.github.com>
2021-01-26 16:43:24 +0200

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -5,8 +5,8 @@ import domain.Exercise;
 public class TestResult {
 
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        int a = 2;
+        int b = 7;
+        System.out.println("Primary numbers from " + a + " to " + b +" : " + Exercise.Calculate(a, b));
     }
 }
